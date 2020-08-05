f9bcdcbc90cbd15fb7598daa207660c761459688
OrangeOnBlack <OrangeOnBlack@users.noreply.github.com>
2020-08-05 17:58:04 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "\nadvertised.listeners=PLAINTEXT://$KAFKA_LISTEN:$KAFKA_PORT" >> /kafka_2.12-2.2.0/config/server.properties
+printf "\nadvertised.listeners=PLAINTEXT://$KAFKA_LISTEN:$KAFKA_PORT" >> /kafka_2.12-2.2.2/config/server.properties
 nohup bin/zookeeper-server-start.sh config/zookeeper.properties </dev/null >/dev/null 2>&1 &
 zookeeperpid=$!
 bin/kafka-server-start.sh config/server.properties
